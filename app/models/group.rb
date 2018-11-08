d1c3a7e8f7a0fd9a3a8cda8f80345deb7fcfb201
yoshi2-2 <netopiku@gmail.com>
2018-11-08 09:18:49 +0900

    Update sidebar message
    last message, "画像が投稿されています","まだメッセージはありません"

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,19 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  validates :name, presence: true
   has_many :messages
+
+  validates :name, presence: true
+
+  def show_last_message
+    if messages.last.present?
+      if messages.last.body?
+        messages.last.body
+      else
+        "画像が投稿されています。"
+      end
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end

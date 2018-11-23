c6889df4d1a105a3c4b0a9a493ba9f799680868b
yoshi2-2 <netopiku@gmail.com>
2018-11-23 12:21:03 +0900

    fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'upload-yoshi'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-yoshi'
+  config.fog_directory  = 'upyoshi'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upyoshi'
 end
